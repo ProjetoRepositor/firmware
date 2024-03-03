7b0866eb3e6f1a0f052b9ed555bc1e2e7d36b150
jokerfoxyy <victorassagra@hotmail.com>
2024-03-03 19:25:31 -0300

    Microfone Funcionando

--- TCC_Code_Caio.py
+++ TCC_Code_Caio.py
@@ -3,6 +3,26 @@ import pyaudio
 import requests
 import numpy as np
 import wave,datetime,os
+import gpiozero
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(21,GPIO.OUT)
+
+GPIO.output(21,GPIO.LOW)
+
+def button_press():
+    record_length =  4
+    data_chunks,data_frames,t_0 = data_grabber(record_length) # grab the data
+    print('Salvando..')
+    file_name= data_saver(t_0, data_frames, data_chunks) # save the data as a .wav file
+    print(f'Salvo {file_name}')
+
+botao = gpiozero.Button(3)
+
+botao.when_pressed = button_press
+
 
 def pyserial_start():
     audio = pyaudio.PyAudio() # create pyaudio instantiation
@@ -22,6 +42,7 @@ def data_grabber(rec_len):
     stream.read(CHUNK,exception_on_overflow=False) # flush port first 
     t_0 = datetime.datetime.now() # get datetime of recording start
     print('Recording Started.')
+    GPIO.output(21, GPIO.HIGH)
     data,data_frames = [],[] # variables
     for frame in range(0,int((samp_rate*rec_len)/CHUNK)):
         # grab data frames from buffer
@@ -29,10 +50,11 @@ def data_grabber(rec_len):
         data_frames.append(stream_data) # append data
         data.append(np.frombuffer(stream_data,dtype=buffer_format))
     stream.stop_stream() # stop data stream
+    GPIO.output(21, GPIO.LOW)
     print('Recording Stopped.')
     return data,data_frames,t_0
 
-def data_saver(t_0):
+def data_saver(t_0, data_frames, data_chunks):
     data_folder = './data/' # folder where data will be saved locally
     if os.path.isdir(data_folder)==False:
         os.mkdir(data_folder) # create folder if it doesn't exist
@@ -53,18 +75,22 @@ def data_saver(t_0):
 
 def transcribe(file_name):
     url = "http://localhost:800/api/audio"
-
+#token 7f4e2164a8e8ef0be122d2cbd1ff61e79d4973cfd29cc9917fb4b8ff3d87bd7b
     with open(f'data/{file_name}.wav', 'rb') as f:
         payload = f.read()
 
     headers = {
-    'Content-Type': 'audio/wave'
+    'Content-Type': 'audio/wave',
+    'token': '7f4e2164a8e8ef0be122d2cbd1ff61e79d4973cfd29cc9917fb4b8ff3d87bd7b'
     }
 
     response = requests.request("POST", url, headers=headers, data=payload)
 
     print(response.text)
 
+
+def loop():
+    pass
 
 
 if __name__=="__main__":
@@ -85,13 +111,11 @@ if __name__=="__main__":
     #############################
     #
     stream,audio = pyserial_start() # start the pyaudio stream   
-    record_length =  7 # seconds to record
+     # seconds to record
     
-    input('Press Enter to Record Data (Turn Freq. Generator On)')
-    data_chunks,data_frames,t_0 = data_grabber(record_length) # grab the data
-    print('Salvando..')
-    file_name= data_saver(t_0) # save the data as a .wav file
-    print(f'Salvo {file_name}')
-    transcribe(file_name)
+    while 1:
+        loop()
+
+    # transcribe(file_name)
     pyserial_end() # close the stream/pyaudio connection
    
