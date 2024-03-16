3c4f8d5f0c0492b2b27504d7c4c5b9caf06f4257
CaioRuizz <caio.ruiz00@gmail.com>
2024-03-16 17:45:12 -0300

    wifi e bluetooh serial

--- main.py
+++ main.py
@@ -3,13 +3,49 @@ import pyaudio
 import requests
 import numpy as np
 import wave,datetime,os
+import gpiozero
+import RPi.GPIO as GPIO
+from serial import Serial
+import time
+import json
+import threading
+import os
+
+tr = threading.Thread(target=lambda: os.system('sudo python wifi.py'))
+tr.start()
+
+ser = Serial ("/dev/ttyS0", 9600) 
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(21,GPIO.OUT)
+
+GPIO.output(21,GPIO.LOW)
+
+def button_press():
+    record_length =  4
+    data_chunks,data_frames,t_0 = data_grabber(record_length) # grab the data
+    print('Salvando..')
+    file_name= data_saver(t_0, data_frames, data_chunks) # save the data as a .wav file
+    print(f'Salvo {file_name}')
+    transcribe(file_name)
+
+botao = gpiozero.Button(3)
+
+botao.when_pressed = button_press
+
 
 def pyserial_start():
     audio = pyaudio.PyAudio() # create pyaudio instantiation
-  
 
-    stream = audio.open(format = pyaudio_format,rate = samp_rate,channels = chans, input_device_index = dev_index,input = True,frames_per_buffer=CHUNK)
-                        
+    # Ajuste o ganho (intensidade do sinal de entrada)
+    stream = audio.open(format = pyaudio_format,
+                        rate = samp_rate,
+                        channels = chans,
+                        input_device_index = dev_index,
+                        input = True,
+                        frames_per_buffer=CHUNK)
+
     stream.stop_stream() # stop stream to prevent overload
     return stream,audio
 
@@ -22,6 +58,7 @@ def data_grabber(rec_len):
     stream.read(CHUNK,exception_on_overflow=False) # flush port first 
     t_0 = datetime.datetime.now() # get datetime of recording start
     print('Recording Started.')
+    GPIO.output(21, GPIO.HIGH)
     data,data_frames = [],[] # variables
     for frame in range(0,int((samp_rate*rec_len)/CHUNK)):
         # grab data frames from buffer
@@ -29,10 +66,11 @@ def data_grabber(rec_len):
         data_frames.append(stream_data) # append data
         data.append(np.frombuffer(stream_data,dtype=buffer_format))
     stream.stop_stream() # stop data stream
+    GPIO.output(21, GPIO.LOW)
     print('Recording Stopped.')
     return data,data_frames,t_0
 
-def data_saver(t_0):
+def data_saver(t_0, data_frames, data_chunks):
     data_folder = './data/' # folder where data will be saved locally
     if os.path.isdir(data_folder)==False:
         os.mkdir(data_folder) # create folder if it doesn't exist
@@ -51,6 +89,49 @@ def data_saver(t_0):
 ##############################################
 #
 
+def transcribe(file_name):
+    url = "http://localhost:800/api/audio"
+#token 7f4e2164a8e8ef0be122d2cbd1ff61e79d4973cfd29cc9917fb4b8ff3d87bd7b
+    with open(f'data/{file_name}.wav', 'rb') as f:
+        payload = f.read()
+
+    headers = {
+    'Content-Type': 'audio/wave',
+    'token': '7f4e2164a8e8ef0be122d2cbd1ff61e79d4973cfd29cc9917fb4b8ff3d87bd7b'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.text)
+
+
+def send_ean(ean):
+    url = "https://rq0ak44zy0.execute-api.sa-east-1.amazonaws.com/Prod/api/v1/carrinho"
+
+    payload = json.dumps({
+        "codigoDeBarras": ean,
+        "quantidade": 1
+    })
+    headers = {
+        'token': '7f4e2164a8e8ef0be122d2cbd1ff61e79d4973cfd29cc9917fb4b8ff3d87bd7b',
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.text)
+
+
+
+def loop():
+    print('Esperando Leitura de código de barras')
+    received_data = ser.read()              #read serial port
+    time.sleep(0.03)
+    data_left = ser.inWaiting()             #check for remaining byte
+    received_data += ser.read(data_left)
+    ean = f'{received_data.decode("utf8").split()[0]}'
+    send_ean(ean)
+    print(ean)
 
 
 if __name__=="__main__":
@@ -71,12 +152,9 @@ if __name__=="__main__":
     #############################
     #
     stream,audio = pyserial_start() # start the pyaudio stream   
-    record_length =  7 # seconds to record
     
-    input('Press Enter to Record Data (Turn Freq. Generator On)')
-    data_chunks,data_frames,t_0 = data_grabber(record_length) # grab the data
-    print('Salvando..')
-    file_name= data_saver(t_0) # save the data as a .wav file
-    print(f'Salvo {file_name}')
+    while 1:
+        loop()
+
     pyserial_end() # close the stream/pyaudio connection
    
---
+++ wifi.py
@@ -0,0 +1,51 @@
+import serial
+import json
+import subprocess
+import os
+
+serial_port = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+
+def configure_wifi(ssid, password):
+    config_lines = [
+        'ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev',
+        'update_config=1',
+        'country=BR',
+        '\n',
+        'network={',
+        '\tssid="{}"'.format(ssid),
+        '\tpsk="{}"'.format(password),
+        '\tkey_mgmt=WPA-PSK',
+        '}'
+        ]
+    config = '\n'.join(config_lines)
+    
+    #give access and writing. may have to do this manually beforehand
+    os.popen("sudo chmod a+w /etc/wpa_supplicant/wpa_supplicant.conf")
+    
+    #writing to file
+    with open("/etc/wpa_supplicant/wpa_supplicant.conf", "w") as wifi:
+        wifi.write(config)
+    
+    print("Wifi config added. Refreshing configs")
+    ## refresh configs
+    os.popen("sudo wpa_cli -i wlan0 reconfigure")
+
+while True:
+    # Verifica se há dados disponíveis para leitura
+    if serial_port.in_waiting > 0:
+        # Lê os dados recebidos da porta serial
+        serial_data = serial_port.readline().decode('utf-8')
+        try:
+            # Tenta interpretar os dados recebidos como um JSON
+            data = json.loads(serial_data)
+            print(data)
+            ssid = data.get('ssid')
+            password = data.get('password')
+            token = data.get('token')
+            if ssid and password and token:
+                # Configura o Wi-Fi com os dados recebidos
+                configure_wifi(ssid, password)
+                print(os.system(f'echo "{token}" > ~/token.txt'))
+        except json.JSONDecodeError:
+            print("Erro ao decodificar JSON")
+
